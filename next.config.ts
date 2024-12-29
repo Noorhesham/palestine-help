8cf7fe732c93a0863426293a3c4138ac522214b4
Noorhesham <noordragon2004@gmail.com>
2024-12-29 08:31:42 +0200

    meow

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
-  output: "export",
-  images: { unoptimized: true },
 };
 
 export default withNextIntl(nextConfig);
